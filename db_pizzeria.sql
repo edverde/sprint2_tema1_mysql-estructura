722417cfdb88a38e4195130bcd8be588b950a419
edverde <eduardvr83@gmail.com>
2022-04-24 13:09:05 +0200

    valors afegits

--- db_pizzeria.sql
+++ db_pizzeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-04-2022 a las 00:33:08
+-- Tiempo de generación: 24-04-2022 a las 13:07:32
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.27
 
@@ -32,6 +32,14 @@ CREATE TABLE `categoria_empleat` (
   `nom_categoria` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `categoria_empleat`
+--
+
+INSERT INTO `categoria_empleat` (`id_categoria_empleat`, `nom_categoria`) VALUES
+(1, 'cuiner'),
+(2, 'repartidor');
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +50,17 @@ CREATE TABLE `categoria_pizza` (
   `id_categoria_pizza` int(11) NOT NULL,
   `nom` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria_pizza`
+--
+
+INSERT INTO `categoria_pizza` (`id_categoria_pizza`, `nom`) VALUES
+(1, 'carbonara'),
+(2, 'barbacoa'),
+(3, 'vegetal'),
+(4, 'formatges'),
+(5, 'atun');
 
 -- --------------------------------------------------------
 
@@ -57,6 +76,15 @@ CREATE TABLE `clients` (
   `telefon` varchar(15) DEFAULT NULL,
   `localitats_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clients`
+--
+
+INSERT INTO `clients` (`id_clients`, `nom`, `direccio`, `cp`, `telefon`, `localitats_id`) VALUES
+(1, 'Eduard', 'anselm_clave_13_segon', '08902', '68654654', 1),
+(2, 'sonia', NULL, '08902', NULL, 2),
+(3, 'breda', NULL, NULL, NULL, 7);
 
 -- --------------------------------------------------------
 
@@ -75,6 +103,15 @@ CREATE TABLE `comandes` (
   `data_hora_entrega` datetime DEFAULT NULL,
   `tenda_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comandes`
+--
+
+INSERT INTO `comandes` (`id_comanda`, `data_hora`, `envio`, `quantitat_producte`, `preu_total`, `clients_id`, `empleat_repartidor_id`, `data_hora_entrega`, `tenda_id`) VALUES
+(1, '2022-04-15 12:53:18', 'domicili', 6, '30.00', 1, 1, '2022-04-23 12:55:18', 2),
+(2, '2022-04-24 12:53:17', NULL, NULL, NULL, 1, 1, '2022-04-24 12:53:17', 2),
+(3, '2022-04-24 12:58:01', NULL, NULL, NULL, 2, 9, '2022-04-24 12:58:01', 5);
 
 -- --------------------------------------------------------
 
@@ -103,6 +140,22 @@ CREATE TABLE `empleats` (
   `categoria_empleat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `empleats`
+--
+
+INSERT INTO `empleats` (`id_empleat`, `nom`, `cognom`, `nif`, `telefon`, `tenda_id`, `categoria_empleat_id`) VALUES
+(1, 'alberto', 'caraja', '44556678O', '45645645', 1, 1),
+(2, 'ruben', 'herrera', '44559988R', '654987987', 1, 2),
+(3, 'juanjo', 'cardenal', '45678912e', '654321654', 2, 1),
+(4, 'maria', 'caca', '4567891r', '654987789', 2, 2),
+(5, 'rodolfo', 'rewrwrew', '4564654k', '65445646', 6, 2),
+(6, 'carolina', 'pichardo', '456789456', '567894561g', 7, 1),
+(7, 'alfred', 'kk', '456546rf', '546456456', 10, 2),
+(8, 'maria', 'unpajote', '4567891e', '456123458', 9, 1),
+(9, 'dolores', 'cabeza', '456789t', '456123', 9, 2),
+(10, 'nuria', 'gonzalez', '456789123', '44123575t', 3, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -114,6 +167,22 @@ CREATE TABLE `localitat` (
   `nom` varchar(50) DEFAULT NULL,
   `provincia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `localitat`
+--
+
+INSERT INTO `localitat` (`id_localitat`, `nom`, `provincia_id`) VALUES
+(1, 'hospitalet_de_llobregat', 2),
+(2, 'santa_coloma', 2),
+(3, 'barcelona', 2),
+(4, 'Besalu', 3),
+(5, 'breda', 3),
+(6, 'pals', 3),
+(7, 'cambrils', 4),
+(8, 'reus', 4),
+(9, 'torregrossa', 5),
+(10, 'mollerussa', 5);
 
 -- --------------------------------------------------------
 
@@ -142,6 +211,16 @@ CREATE TABLE `provincies` (
   `nom` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `provincies`
+--
+
+INSERT INTO `provincies` (`id_provincies`, `nom`) VALUES
+(2, 'barcelona'),
+(3, 'girona'),
+(4, 'tarragona'),
+(5, 'lleida');
+
 -- --------------------------------------------------------
 
 --
@@ -155,6 +234,22 @@ CREATE TABLE `tenda` (
   `localitat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `tenda`
+--
+
+INSERT INTO `tenda` (`id_tenda`, `direccio`, `cp`, `localitat_id`) VALUES
+(1, 'prat_de_la_riba_166', '08907', 1),
+(2, 'muns_26', '08902', 1),
+(3, 'America_101', '08924', 2),
+(4, 'carme_134', '05678', 4),
+(5, 'santa_eulalia_12', '08956', 6),
+(6, 'Ramon_i_cajal_8', '08965', 9),
+(7, 'marie_curie_54', '08975', 10),
+(8, 'unio_56', '08902', 1),
+(9, 'tallers_26', '08925', 3),
+(10, 'gran_130', '09865', 6);
+
 -- --------------------------------------------------------
 
 --
@@ -165,6 +260,15 @@ CREATE TABLE `tipus` (
   `id_tipus` int(11) NOT NULL,
   `nom` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipus`
+--
+
+INSERT INTO `tipus` (`id_tipus`, `nom`) VALUES
+(1, 'pizza'),
+(2, 'burguer'),
+(3, 'beguda');
 
 --
 -- Índices para tablas volcadas
@@ -257,61 +361,61 @@ ALTER TABLE `tipus`
 -- AUTO_INCREMENT de la tabla `categoria_empleat`
 --
 ALTER TABLE `categoria_empleat`
-  MODIFY `id_categoria_empleat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria_empleat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria_pizza`
 --
 ALTER TABLE `categoria_pizza`
-  MODIFY `id_categoria_pizza` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria_pizza` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id_clients` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_clients` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `comandes`
 --
 ALTER TABLE `comandes`
-  MODIFY `id_comanda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_comanda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `empleats`
 --
 ALTER TABLE `empleats`
-  MODIFY `id_empleat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_empleat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `localitat`
 --
 ALTER TABLE `localitat`
-  MODIFY `id_localitat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_localitat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `productes`
 --
 ALTER TABLE `productes`
-  MODIFY `id_productes` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_productes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `provincies`
 --
 ALTER TABLE `provincies`
-  MODIFY `id_provincies` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_provincies` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tenda`
 --
 ALTER TABLE `tenda`
-  MODIFY `id_tenda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `tipus`
 --
 ALTER TABLE `tipus`
-  MODIFY `id_tipus` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tipus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
